f8ed80f7949bea1a0cb5ef42a2bbf7f76e3c6502
bryce <bgt17@txstate.edu>
2023-10-21 14:18:26 -0500

    sidebar

--- web/src/components/SideBar.jsx
+++ web/src/components/SideBar.jsx
@@ -10,21 +10,41 @@ const SideBar = () => {
     return ( 
 
         <div>
-            <AiOutlineMenu onClick={handleNav }className='absolute top-4 right-4 z-[99] md:hidden'/>
-            {
-                nav ? (
-                    <div className='fixed w-full hscreen bg-white/90 flex flex-col justify-center items-center z-20'>
-                        <a href="#main" className='w-[75%] flex justify-center'>
+            <AiOutlineMenu 
+            onClick={handleNav }
+            className='absolute top-4 right-4 z-[99] md:hidden'
+            />
+            {nav ? (
+                    <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
+                        >    
                             <AiOutlineHome size={20} />
                             <span className='pl-4'>Home</span>
-
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
+                        >    
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
+                        >    
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
+                        >    
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+                        </a>
+                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
+                        >    
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
                         </a>
                     </div>
-                )
-                : (
+                ): (
                     <div> </div>
-                )
-            }
+                )}
         </div>
      );
 };
