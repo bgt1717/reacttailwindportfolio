e6c2c1cf578f0fced8f85ef3c33bbea2517bb695
bryce <bgt17@txstate.edu>
2023-10-21 14:33:39 -0500

    sidebar

--- web/src/components/SideBar.jsx
+++ web/src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai';
+import {AiOutlineMenu, AiOutlineHome, AiOutlineProject,AiOutlineMail} from 'react-icons/ai';
+import {GrProjects} from 'react-icons/gr'
 
 const SideBar = () => {
     const[nav, setNav] = useState(false);
@@ -23,17 +24,17 @@ const SideBar = () => {
                         </a>
                         <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
-                            <AiOutlineHome size={20} />
+                            <GrProjects size={20} />
                             <span className='pl-4'>About </span>
                         </a>
                         <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
-                            <AiOutlineHome size={20} />
+                            <AiOutlineProject size={20} />
                             <span className='pl-4'> Projects</span>
                         </a>
                         <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
-                            <AiOutlineHome size={20} />
+                            <AiOutlineMail size={20} />
                             <span className='pl-4'>Contact</span>
                         </a>
                     </div>
