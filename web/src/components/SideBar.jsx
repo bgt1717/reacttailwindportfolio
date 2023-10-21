90fe9aeef5646ceba96164df04c9e35c30e12137
bryce <bgt17@txstate.edu>
2023-10-21 14:24:53 -0500

    sidebar update

--- web/src/components/SideBar.jsx
+++ web/src/components/SideBar.jsx
@@ -24,22 +24,17 @@ const SideBar = () => {
                         <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
                             <AiOutlineHome size={20} />
-                            <span className='pl-4'>Home</span>
+                            <span className='pl-4'>About </span>
                         </a>
                         <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
                             <AiOutlineHome size={20} />
-                            <span className='pl-4'>Home</span>
+                            <span className='pl-4'> Projects</span>
                         </a>
                         <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
                             <AiOutlineHome size={20} />
-                            <span className='pl-4'>Home</span>
-                        </a>
-                        <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
-                        >    
-                            <AiOutlineHome size={20} />
-                            <span className='pl-4'>Home</span>
+                            <span className='pl-4'>Contact</span>
                         </a>
                     </div>
                 ): (
