fe81bfb23ee0dd0177230feb5b9a4584c0ba3af6
bryce <bgt17@txstate.edu>
2023-10-21 13:45:56 -0500

    sidebar update

--- web/src/components/SideBar.jsx
+++ web/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {AiOutlineMenu} from 'react-icons/ai';
+import {AiOutlineMenu, AiOutlineHome} from 'react-icons/ai';
 
 const SideBar = () => {
     const[nav, setNav] = useState(false);
@@ -10,12 +10,23 @@ const SideBar = () => {
     return ( 
 
         <div>
-            <AiOutlineMenu className='absolute top-4 right-4 z-[99] md:hidden'/>
+            <AiOutlineMenu onClick={handleNav }className='absolute top-4 right-4 z-[99] md:hidden'/>
+            {
+                nav ? (
+                    <div className='fixed w-full hscreen bg-white/90 flex flex-col justify-center items-center z-20'>
+                        <a href="#main" className='w-[75%] flex justify-center'>
+                            <AiOutlineHome size={20} />
+                            <span className='pl-4'>Home</span>
+
+                        </a>
+                    </div>
+                )
+                : (
+                    <div> </div>
+                )
+            }
         </div>
-
-
-
      );
-}
+};
  
 export default SideBar;
