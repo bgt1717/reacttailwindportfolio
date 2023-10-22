039ba1880672f345567c9690b15baee2974ccee7
bryce <bgt17@txstate.edu>
2023-10-21 21:56:13 -0500

    Home update

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -7,8 +7,9 @@ return(
 <div id ='main'>
   <img className=' w-full h-screen object-cover object-right' src="https://images.unsplash.com/photo-1531218150217-54595bc2b934?auto=format&fit=crop&q=80&w=1228&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
   <div className=' w-full h-screen absolute top-0 left-0 bg-white/50'>
-  <div>
-      <h1>Hello. I'm Bryce.</h1>
+  <div className=' max-w-[700px] m-auto h-full w-full flex flex-col justify-center log:items-start items-center' >
+      {/* Anything above small text 5xl otherwise text 4xl. 100 through 800 larger number is darker font.*/}
+      <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Hello. I'm Bryce.</h1>
       <h2> I'm a 
           <TypeAnimation
           sequence={[
@@ -28,7 +29,7 @@ return(
 
       </h2>
       <div>
-        <FaLinkedinIn />
+        <FaLinkedinIn className='cursor-pointer' size={20}/>
       </div>
   </div>
   </div>
