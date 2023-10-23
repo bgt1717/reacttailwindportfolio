559d0b0deacefc2ed513d105a92b7e7f58fe2a66
bryce <bgt17@txstate.edu>
2023-10-22 22:59:32 -0500

    projects update

---
+++ web/src/components/ProjectItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProjectItem() {
+  return (
+    <>
+      <div>project item</div>
+    </>
+  );
+}
+
+export default ProjectItem;
--- web/src/components/Projects.jsx
+++ web/src/components/Projects.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from 'react'
+import ProjectItem from './ProjectItem'
+import ProjectItemPic from '../assets/360.jpg'
+import ProjectItemPic2 from '../assets/atx.jpg'
+import ProjectItemPic3 from '../assets/congress.jpg'
+import ProjectItemPic4 from '../assets/congressbuild.jpg'
+
 
 function Projects() {
   return (
     <>
-      <div>Projects</div>
+      <div id='projects' className='max-w-[1040px] m-auto '>Projects</div>
     </>
   );
 }
