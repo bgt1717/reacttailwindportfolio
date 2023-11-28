4f7e945817b5a0d60012ad959cfa4e4de5635ebf
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-11-28 12:07:14 -0600

    update

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -3,43 +3,40 @@ import { TypeAnimation } from 'react-type-animation';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { AiFillGithub } from "react-icons/ai";
 import atx from '../assets/atx.jpg';
-// import mepic from '../assets/me.jpg';
 
 const Home = () => {
   return (
-    <div id='Home' className="flex">
+    <div id='Home' className="flex items-center justify-center">
       <img className='w-full h-screen object-cover object-right' src={atx} alt="picture of city" />
-      <div className='w-full h-screen absolute top-0 left-0 bg-white/50 flex'>
-        <div className='w-1/4 m-auto h-full w-full flex flex-col justify-center items-start'>
-          <div className="bg-white p-10 rounded-xl">
-            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Hello. I'm Bryce.</h1>
-            <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800 '>I'm a
-              <TypeAnimation
-                sequence={[
-                  'programmer.',
-                  1000,
-                  'website developer.',
-                  1000,
-                  'React enthusiast.',
-                  1000,
-                ]}
-                wrapper="span"
-                speed={50}
-                style={{ fontSize: '1em', paddingLeft: "5px" }}
-                repeat={Infinity}
-              />
-            </h2>
+      <div className='w-full h-screen absolute top-0 left-0 bg-white/50 flex items-center justify-center'>
+        <div className='w-1/2 bg-white p-8 rounded-xl flex flex-col items-center'>
+          <h1 className='sm:text-5xl text-4xl font-bold text-gray-800 text-center'>Hello. I'm Bryce.</h1>
+          <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800 text-center'>I'm a
+            <TypeAnimation
+              sequence={[
+                'programmer.',
+                1000,
+                'website developer.',
+                1000,
+                'React enthusiast.',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: '1em', paddingLeft: "5px" }}
+              repeat={Infinity}
+            />
+          </h2>
 
-            <div className='flex justify-between pt-6 max-w-[100px] w-full'>
-              <a href="https://www.linkedin.com/in/bryce-townsend-426289237/" target="_blank" rel="noopener noreferrer">
-                <FaLinkedinIn className='cursor-pointer' size={20} />
-              </a>
-              <a href="https://github.com/bgt1717" target="_blank" rel="noopener noreferrer">
-                <AiFillGithub className='cursor-pointer' size={20} />
-              </a>
-            </div>
-
+          <div className='flex justify-between pt-6 max-w-[100px] w-full'>
+            <a href="https://www.linkedin.com/in/bryce-townsend-426289237/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedinIn className='cursor-pointer' size={20} />
+            </a>
+            <a href="https://github.com/bgt1717" target="_blank" rel="noopener noreferrer">
+              <AiFillGithub className='cursor-pointer' size={20} />
+            </a>
           </div>
+
         </div>
       </div>
     </div>
--- web/src/components/Projects.jsx
+++ web/src/components/Projects.jsx
@@ -8,7 +8,7 @@ import ProjectItemPic4 from '../assets/congressbuild.jpg';
 function Projects() {
   return (
     <div className="bg-blue-200">
-      <div id='Projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-4 pb-10'>
+      <div id='Projects' className='max-w-[1040px] m-auto md:pl-10 p-4 py-4 pb-10'>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
         {/* <p className='text-center text-2xl py-8'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quas vel quod. Esse eveniet error exercitationem iste consequatur porro sit dolore possimus odit dolorem, reprehenderit laudantium autem incidunt consequuntur eos?
@@ -17,9 +17,9 @@ function Projects() {
       {/* Create a grid with two rows and two columns */}
       <div className='grid grid-cols-2 gap-4'>
         <ProjectItem img={ProjectItemPic} title='360 App' />
-        <ProjectItem img={ProjectItemPic2} title='ATX App' />
-        <ProjectItem img={ProjectItemPic3} title='Congress App' />
-        <ProjectItem img={ProjectItemPic4} title='Congress Building App' />
+        <ProjectItem img={ProjectItemPic} title='ATX App' />
+        <ProjectItem img={ProjectItemPic} title='Congress App' />
+        <ProjectItem img={ProjectItemPic} title='Congress Building App' />
       </div>
     </div>
   );
