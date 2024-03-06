8d68266d29100dc7bf61290fb2436af6f5961cfe
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-03-05 19:43:32 -0600

    sidebar updated

--- web/src/components/SideBar.jsx
+++ web/src/components/SideBar.jsx
@@ -10,10 +10,11 @@ const SideBar = () => {
     };
     return ( 
 
+        // -----------------HAMBURGER MENU ------------------------------------------ 
         <div>
             <AiOutlineMenu 
             onClick={handleNav }
-            className='absolute top-4 right-4 z-[99] md:hidden'
+            className='absolute top-4 right-4 z-[99] md:hidden text-6xl'
             />
             {nav ? (
                     <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
@@ -27,7 +28,7 @@ const SideBar = () => {
                             <GrProjects size={20} />
                             <span className='pl-4'>About </span>
                         </a>
-                        <a onClick={handleNav} href="#About" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
+                        <a onClick={handleNav} href="#Skills" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 p-4 m-2 cursor-pointer hover:scale-110 ease-in duration-200'
                         >    
                             <AiOutlineSolution size={20} />
                             <span className='pl-4'>Skills </span>
@@ -47,8 +48,8 @@ const SideBar = () => {
                     <div> </div>
                 )}
                 
-                {/* ----------------------------------------------------------- */}
-                <div className='md:block hidden fixed top-[25%] z-10'>
+                {/* -----------------LEFT MENU ------------------------------------------ */}
+                <div className='md:block hidden fixed top-[5%] z-10'>
                     <div className='flex flex-col'>
                         <a href='#Home' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                             <AiOutlineHome  size={30}/>
