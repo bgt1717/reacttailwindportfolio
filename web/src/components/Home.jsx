2d1834086f73e02f23f9ee7a7afa083f755eb436
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2024-03-05 19:48:36 -0600

    format updated

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -31,13 +31,17 @@ const Home = () => {
               repeat={Infinity}
             />
           </h2>
-          <a href="https://drive.google.com/file/d/12cinhmut1lt1kOMPSM6Mwr2ZcJ_Fhjuu/view" target="_blank" rel="noopener noreferrer">
-    <button class="mt-4 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
-        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Resume
-        </span>
-    </button>
-</a>
+          <a
+            href="https://drive.google.com/file/d/12cinhmut1lt1kOMPSM6Mwr2ZcJ_Fhjuu/view"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button class="mt-4 relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Resume
+              </span>
+            </button>
+          </a>
 
           <div className="flex justify-between pt-4 max-w-[100px] w-full">
             <a
