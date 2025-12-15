de7fcd29d08f9891c928a41b451c8bd91d96f4bc
bgt1717 <brycetown10@gmail.com>
2025-12-14 22:32:40 -0600

    updates

--- web/src/components/ProjectItem.jsx
+++ web/src/components/ProjectItem.jsx
@@ -31,7 +31,7 @@ const ProjectItem = ({ img, title, githubUrl, websiteDemo, demoDownloadUrl }) =>
               className="text-gray-900 bg-gradient-to-r from-green-200 via-green-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-900 dark:focus:ring-green-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center transition duration-200"
               role="button"
             >
-              Download Executable
+              Download
             </div>
           </a>
         )}
