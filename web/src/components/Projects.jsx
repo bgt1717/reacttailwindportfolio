c1c24699c49aeec8e29d92621ec5c225aec52cbf
bgt1717 <brycetown10@gmail.com>
2026-01-11 23:15:12 -0600

    Project changes

--- web/src/components/Projects.jsx
+++ web/src/components/Projects.jsx
@@ -12,9 +12,9 @@ function Projects() {
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
       </div>
       <div className='grid grid-cols md:grid-cols-2 gap-4 lg:px-60 md:px-40 sm:px-32'>
-        <ProjectItem img={ProjectItemPic} title='Gantt App' githubUrl='https://github.com/bgt1717/GantAPP/' websiteDemo='https://ganttapp-pe5h.onrender.com/login/'/>
+        <ProjectItem img={ProjectItemPic} title='Gantt App' githubUrl='https://github.com/bgt1717/GantAPP/' websiteDemo='https://ganttapp-pe5h.onrender.com/'/>
         <ProjectItem img={ProjectItemPic2} title='Bag Website' githubUrl='https://github.com/bgt1717/BagWebsite2/tree/main/BagSite' websiteDemo='https://atxbags.onrender.com/'/>
-        <ProjectItem img={ProjectItemPic3} title='Notes APp' githubUrl='https://github.com/bgt1717/CRUD_Project_VB' websiteDemo='https://notesapp-s8ix.onrender.com/'/>
+        <ProjectItem img={ProjectItemPic3} title='Notes App' githubUrl='https://github.com/bgt1717/CRUD_Project_VB' websiteDemo='https://notesapp-s8ix.onrender.com/'/>
         <ProjectItem img={ProjectItemPic} title='LeetCode Progression ' githubUrl='https://github.com/bgt1717/Practice_LeetCode' />
         {/* Add more ProjectItem components as needed */}
       </div>
