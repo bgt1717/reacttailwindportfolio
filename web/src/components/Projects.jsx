fd9ecf978c3e009702d6d2129c6e553b5386d656
bgt1717 <brycetown10@gmail.com>
2026-01-24 18:26:40 -0600

    Projects update

--- web/src/components/Projects.jsx
+++ web/src/components/Projects.jsx
@@ -3,6 +3,7 @@ import ProjectItem from "./ProjectItem";
 import ProjectItemPic from "../assets/gantt.jpg";
 import ProjectItemPic2 from "../assets/bag.avif";
 import ProjectItemPic3 from "../assets/NotesApp.jpg";
+import ProjectItemPic4 from "../assets/360.jpg";
 
 function Projects() {
   return (
@@ -37,6 +38,17 @@ function Projects() {
 
           <ProjectItem
             img={ProjectItemPic3}
+            title="Notes Application"
+            stack="JavaScript, Node.js, Express, MySQL"
+            bullets={[
+              "Built a CRUD-based notes application with a relational database backend.",
+              "Designed REST endpoints to support data persistence and user interactions.",
+            ]}
+            githubUrl="https://github.com/bgt1717/CRUD_Project_VB"
+            websiteDemo="https://notesapp-s8ix.onrender.com/"
+          />
+          <ProjectItem
+            img={ProjectItemPic4}
             title="Notes Application"
             stack="JavaScript, Node.js, Express, MySQL"
             bullets={[
